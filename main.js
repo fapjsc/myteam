7c68e8c449600a5c0d0f7ab406fa88b62f956a1b
fapjsc <p82szh@gmail.com>
2021-02-05 04:57:57 +0800

    form validation

---
+++ main.js
@@ -0,0 +1,45 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      name: { val: '', isValid: true },
+      email: { val: '', isValid: true },
+      message: { val: '', isValid: true },
+      formIsValid: true,
+      emailRule: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+    };
+  },
+
+  methods: {
+    validation() {
+      if (this.name.val === '') {
+        this.name.isValid = false;
+        this.formIsValid = false;
+      }
+
+      if (this.email.val.search(this.emailRule) == -1) {
+        this.email.isValid = false;
+        this.formIsValid = false;
+      }
+
+      if (this.message.val === '') {
+        this.message.isValid = false;
+        this.formIsValid = false;
+      }
+    },
+
+    submitForm() {
+      this.validation();
+      if (!this.formIsValid) {
+        return;
+      } else {
+        alert('submit successfully');
+      }
+    },
+
+    clearErr(input) {
+      input.isValid = true;
+    },
+  },
+});
+
+app.mount('#app');
